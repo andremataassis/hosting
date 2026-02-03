d04283c5d877c4e052e94be34da68f9e6d58d2d2
AndreMAssis <andremataassis@gmail.com>
2026-02-02 23:55:45 -0500

    Update game.js

--- resources/dev3/game.js
+++ resources/dev3/game.js
@@ -321,7 +321,7 @@ PS.init = function( system, options ) {
 
 	// Add any other initialization code you need here.
 	PS.timerStart(4, updateGame);
-	PS.debug("Draw something and hit play!\n\nPlay around and have fun!\nMade by Andre Mata Assis.");
+	PS.debug("Draw something and hit play!\n\nMade by Andre Mata Assis.");
 };
 
 /*
